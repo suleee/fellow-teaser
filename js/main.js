3bf736d5af49b4bb7365ddc629a12f8a59f452cd
Jisu Lee <jaejisu@gmail.com>
2018-01-07 15:14:22 -0800

    Added classes slider

--- js/main.js
+++ js/main.js
@@ -30,6 +30,38 @@ $(function () {
   });
 
 
+  $(document).ready(function(){
+    var $pre = $('.pre-section3');
+    $pre.addClass('idx-0');
+    var $arrowLeft = $('.section3 .arrow-left');
+    var $arrowRight = $('.section3 .arrow-right');
+    var $iconsList = $('.section3 .icon-list');
+    var $sliderSel = $('.class-content');
+    $sliderSel.slick({
+      dots: false,
+      nextArrow: $arrowRight,
+      prevArrow: $arrowLeft,
+      // infinite: true,
+      // speed: 500,
+      fade: true,
+      cssEase: 'linear',
+    });
+
+    $sliderSel.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+      $('.pre-section3').removeClass (function (index, className) {
+        return (className.match (/(^|\s)idx-\S+/g) || []).join(' ');
+      });
+      var nextName = $('.section3 .slick-slide[data-slick-index="' + nextSlide + '"]').attr('data-name');
+      $pre.addClass('idx-' + nextName);
+    });
+    
+    $iconsList.find('li').on('click', function(){
+      var name = $(this).attr('class');
+      var index = $('.section3 .slick-slide[data-name="' + name + '"]').attr('data-slick-index');
+      $sliderSel.slick('slickGoTo', index, false);
+    });
+  });
+
   // items
   // $(".item-list").flickity({
   //   cellAlign: 'center',
